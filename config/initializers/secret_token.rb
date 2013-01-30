89a29b550c82a54de0bbd2079df0447535680cee
Anum Qudsia <anumqudsia@hotmail.com>
2013-01-30 10:29:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecom::Application.config.secret_token = 'cfb642aa22a8d6e98d193f4b1a5a730d66603d9453c6e09b3e7eff3fb778bb3ce8c737ea541e3912dc01331142c1984d04ed79fe3d9a34ee44e618d4e0f396c7'
