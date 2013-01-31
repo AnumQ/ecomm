572a21f673ef182279c41891b5dad91da0d517e0
Anum Qudsia <anumqudsia@hotmail.com>
2013-01-31 18:21:12 +0000

    application.rb change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,10 +19,6 @@ Ecom::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  
-  # Anum's added configurations
-  config.assets.initialize_on_precompile = false
-  config.assets.precompile += %w( style.css )
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
