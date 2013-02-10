dcd477f4a2af79cecd573682ebde3659a4c908ee
Anum Qudsia <anumqudsia@hotmail.com>
2013-02-10 19:01:01 +0000

    compass added

--- config/compass.rb
+++ config/compass.rb
@@ -1,1 +1,6 @@
 additional_import_paths = ["app/assets/stylesheets/basics", "app/assets/stylesheets/shared"]
+
+project_type = :rails
+http_path    = '/'
+css_dir      = 'tmp/stylesheets'
+sass_dir     = 'app/views/stylesheets'
